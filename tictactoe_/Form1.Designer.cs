7e58c59c2dc88e8e299a3c34462a1634e852dca1
senya141 <senya.pupkin11@gmail.com>
2024-10-20 20:24:13 +0300

    Добавьте файлы проекта.

---
+++ tictactoe_/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace tictactoe_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            rulesButton = new Button();
+            restartButton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(707, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // rulesButton
+            // 
+            rulesButton.Location = new Point(697, 73);
+            rulesButton.Name = "rulesButton";
+            rulesButton.Size = new Size(75, 23);
+            rulesButton.TabIndex = 1;
+            rulesButton.Text = "Правила";
+            rulesButton.UseVisualStyleBackColor = true;
+            rulesButton.Click += rulesButton_Click;
+            // 
+            // restartButton
+            // 
+            restartButton.Location = new Point(697, 102);
+            restartButton.Name = "restartButton";
+            restartButton.Size = new Size(75, 23);
+            restartButton.TabIndex = 2;
+            restartButton.Text = "button1";
+            restartButton.UseVisualStyleBackColor = true;
+            restartButton.Click += restartButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(restartButton);
+            Controls.Add(rulesButton);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button rulesButton;
+        private Button restartButton;
+    }
+}
---
+++ tictactoe_/Form1.cs
@@ -0,0 +1,20 @@
+namespace tictactoe_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rulesButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
